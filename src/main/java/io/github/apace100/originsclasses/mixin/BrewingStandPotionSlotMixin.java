f1952eba0c401737c4ea0a300b50f899e6a7a83e
Apace <apace100@web.de>
2021-06-13 17:01:13 +0200

    Prevented extended potions from being brewed

---
+++ src/main/java/io/github/apace100/originsclasses/mixin/BrewingStandPotionSlotMixin.java
@@ -0,0 +1,18 @@
+package io.github.apace100.originsclasses.mixin;
+
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(targets = "net/minecraft/screen/BrewingStandScreenHandler$PotionSlot")
+public class BrewingStandPotionSlotMixin {
+
+    @Inject(method = "matches", at = @At("HEAD"), cancellable = true)
+    private static void preventBrewingExtendedPotions(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        if(stack.getOrCreateTag().getBoolean("IsExtendedByCleric")) {
+            cir.setReturnValue(false);
+        }
+    }
+}
